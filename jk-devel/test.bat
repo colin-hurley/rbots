43457c348631ddcf1d526bb06e7fe37761e99f69
Colin Hurley <darthslaw@gmail.com>
2022-09-24 13:51:49 -0400

    Improve test.bat scripts to be more user-friendly
    The scripts will assume default install path if the JK_HOME/JKM_HOME variable is not set.
    
    The scripts will also display a nice error message if the game executable is not found. To aid in troubleshooting, the message will include the full path to the executable it is trying to execute.

--- jk-devel/test.bat
+++ jk-devel/test.bat
@@ -1,3 +1,16 @@
 @echo off
 
+setlocal
+
+rem If JK_HOME is not set, try the default install path
+if "%JK_HOME%" == "" (
+	set JK_HOME=C:\Program Files\LucasArts\Jedi Knight
+)
+
+rem Ensure game executable exists
+if not exist "%JK_HOME%\JK.EXE" (
+	echo Error: %JK_HOME%\JK.EXE not found
+	goto :EOF
+)
+
 start "Jedi Knight Devel" /D"%JK_HOME%" JK.EXE -devmode -debug log -verbose 2 -windowgui -path %~dps0 %* || pause
--- mots-devel/test.bat
+++ mots-devel/test.bat
@@ -1,3 +1,16 @@
 @echo off
 
+setlocal
+
+rem If JKM_HOME is not set, try the default install path
+if "%JKM_HOME%" == "" (
+	set JKM_HOME=C:\Program Files\LucasArts\MotS
+)
+
+rem Ensure game executable exists
+if not exist "%JKM_HOME%\JKM.EXE" (
+	echo Error: %JKM_HOME%\JKM.EXE not found
+	goto :EOF
+)
+
 start "Mysteries of the Sith Devel" /D"%JKM_HOME%" JKM.EXE -devmode -debug log -verbose 2 -windowgui -path %~dps0 %* || pause
